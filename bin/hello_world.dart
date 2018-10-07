95970f42930d58795c8f675016c8c6aae402ac87
Heikki K Lappalainen <heikkiklappalainen@gmail.com>
2018-10-07 10:04:18 +0300

    Deleted bin folder, sync used-packages to my other packages.

--- bin/hello_world.dart
+++
@@ -1,8 +0,0 @@
-///  dawo-dartlang package by Heikki K. Lappalainen 10.9.2017
-///  dawo version: 0.0.5  6.11.2017
-//
-
-/// hello-world and m ain method
-void main() {
-  print('\n Hello world from version 0.0.5 dawo package! \n');
-}
